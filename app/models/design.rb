53ff8e38eef40edf96ac2dc4222c345495040edc
Hana Yagi <Hana@Hanas-MacBook-Pro.local>
2017-01-27 22:31:37 +0900

    post data save function

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,6 +1,7 @@
 class Design < ActiveRecord::Base
   belongs_to :user
   has_many :images
+  accepts_nested_attributes_for :images
   has_and_belongs_to_many :tags
   acts_as_taggable_on :moods, :colors, :roomtypes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :designs, except: [:show]
   root 'designs#index'
-  get 'designs' => 'designs#index'
-  get 'designs/new' => 'designs#new'
   get 'designs/search' => 'designs#search'
   get 'designs/categories' => 'designs#categories'
   get 'users/mypage' => 'users#mypage'
