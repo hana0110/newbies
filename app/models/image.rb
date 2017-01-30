83f87b8a9de93eb550e495b8012aa0c1e8578639
Hana Yagi <Hana@Hanas-MacBook-Pro.local>
2017-01-31 01:02:47 +0900

    image test uploaded, paperclip size setting edited

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :design, dependent: :destroy
 
   has_attached_file :image,
-      styles:  { medium: "350x250#", thumb: "100x100#" }
+      styles:  { large: "900x650#", thumb: "100x100#" }
   validates_attachment_content_type :image,
     content_type: ["image/jpg","image/jpeg","image/png"]
 end
