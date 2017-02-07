00ddccca8e45dde4825f52c9a85e38db1d4a9dc6
Hana Yagi <Hana@Hanas-MacBook-Pro.local>
2017-02-07 19:32:34 +0900

    AWS fixed

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,12 @@ class Image < ActiveRecord::Base
 
   has_attached_file :image,
       :styles => { large: "900x650#" },
-      
+      :download,
+                    :storage => :s3,
+                    :s3_permissions => :private,
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :path => ":class/:attachment/:id/:style.:extension"
+
   validates_attachment_content_type :image,
     content_type: ["image/jpg","image/jpeg","image/png"]
 
